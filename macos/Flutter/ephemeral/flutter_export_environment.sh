e1d0ecdb33486966745c922c111dd4fc9ae6e610
dsaauser1 <dsaauser1@gmail.com>
2024-03-18 19:15:26 +0700

    refactor: :art: Updated graphql

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\elriz\Data\Softwares\Developments\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\elriz\Data\Companies\PT DSAA GROUP\satujuta\satujuta-mobile-frontend"
+export "FLUTTER_APPLICATION_PATH=C:\Users\elriz\Data\Companies\PT DSAA GROUP\satujuta\satujuta_mobile_frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.1.2"
-export "FLUTTER_BUILD_NUMBER=0.1.2"
+export "FLUTTER_BUILD_NAME=0.1.4"
+export "FLUTTER_BUILD_NUMBER=0.1.4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
